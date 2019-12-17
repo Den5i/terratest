4a426b09cc6620a01450c07276000eacd58dcda2
DenysMorgun <denys.morhun@gmail.com>
2019-12-17 08:24:32 -0500

    update

--- aws.tf
+++ aws.tf
@@ -3,13 +3,30 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["someowner"] # Canonical
+}
+
 # Create a VPC
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
 }
 
 resource "aws_instance" "web" {
-  # ...
+  ami = "aws_ami"
+  instance_type = "t2.micro"
 
   provisioner "chef" {
     attributes_json = <<EOF
@@ -30,11 +47,11 @@ resource "aws_instance" "web" {
     client_options  = ["chef_license 'accept'"]
     run_list        = ["cookbook::recipe"]
     node_name       = "webserver1"
-    secret_key      = "${file("../encrypted_data_bag_secret")}"
+    secret_key      = "${file("/home/den/.ssh/id_rsa_new")}"
     server_url      = "https://chef.company.com/organizations/org1"
     recreate_client = true
     user_name       = "bork"
-    user_key        = "${file("../bork.pem")}"
+    user_key        = "${file("/home/den/.ssh/id_rsa_new")}"
     version         = "12.4.1"
     # If you have a self signed cert on your chef server change this to :verify_none
     ssl_verify_mode = ":verify_peer"
